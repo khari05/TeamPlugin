ca8e1527a12977a041baf5c3e46d95aab62e2c0e
MrCoderBoy345 <kaarthik.hari@gmail.com>
2018-10-28 15:44:16 -0400

    Updated to 1.5

--- src/main/java/com/mrcoderboy345/teamplugin/MainClass.java
+++ src/main/java/com/mrcoderboy345/teamplugin/MainClass.java
@@ -14,7 +14,12 @@ import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.event.player.PlayerChatEvent;
 import cn.nukkit.event.player.PlayerDeathEvent;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.form.response.FormResponse;
+import cn.nukkit.form.window.FormWindow;
+import cn.nukkit.form.window.FormWindowSimple;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.scheduler.NukkitRunnable;
 import cn.nukkit.utils.TextFormat;
 
 /**
@@ -38,7 +43,9 @@ public class MainClass extends PluginBase implements Listener{
         colorMap.put("red", TextFormat.RED);
         colorMap.put("green", TextFormat.GREEN);
         colorMap.put("aqua", TextFormat.AQUA);
-        colorMap.put("reset", TextFormat.WHITE);
+        colorMap.put("reset", TextFormat.RESET);
+        colorMap.put("none", TextFormat.RESET);
+        colorMap.put(null, TextFormat.RESET);
     }
 
     @Override
@@ -70,11 +77,15 @@ public class MainClass extends PluginBase implements Listener{
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         this.getLogger().info("Command received:" + command.getName());
+        
         if (command.getName().toLowerCase().equals("team")) {
-            Player player = (Player) sender;
+            Player player;
             if (args.length == 2) {
                 player = this.getServer().getPlayer(args[1]);
+            } else {
+                player = (Player) sender;
             }
+               player.despawnFromAll();
                 switch(args[0]){
                     case "red":
                         player.setNameTag(TextFormat.RED + "[Red Team] " + TextFormat.WHITE + player.getName());
@@ -82,42 +93,90 @@ public class MainClass extends PluginBase implements Listener{
                         break;
                     case "blue":
                         player.setNameTag(TextFormat.BLUE + "[Blue Team] " + TextFormat.WHITE + player.getName());
-                        player.namedTag.putString("team", "blue");
+                        player.namedTag.putString("team", "playerblue");
                         break;
-                    case "clear":
+                    case "reset":
                         player.setNameTag(TextFormat.WHITE + player.getName());
                         player.namedTag.putString("team", "");
                         break;
                 }
+                new NukkitRunnable(){
+                    @Override
+                    public void run(){
+                        player.spawnToAll();
+                    }
+                }.runTaskLater(this, 60);
             }
         if (command.getName().toLowerCase().equals("rank")) {
-            Player player = (Player) sender;
-            if (args.length > 0){
-                        if (args[0].equals("admin")){
-                            player.setNameTag(TextFormat.BLUE + "[Admin]" + player.getNameTag());
-                        } else  if (args[0].equals("reset")){
-                            player.setNameTag(TextFormat.WHITE + player.getName());
-                        } else {
-                            player.setNameTag("[" + args[0] + "]" + player.getName());
+            Player player;
+            if (args.length == 2){
+                player = this.getServer().getPlayer(args[1]);
+            } else {
+                player = (Player) sender;
+            }
+            if (sender.isOp()){
+                if (args.length > 0){
+                    player.despawnFromAll();
+                    if (args[0].equals("admin")){
+                        player.setNameTag(TextFormat.BLUE + "[Admin]" + colorMap.get(player.namedTag.getString("chatcolor")) + player.namedTag.getString("nickname"));
+                        player.namedTag.putString("rank", "admin");
+                    } else  if (args[0].equals("reset")){
+                        player.setNameTag(TextFormat.WHITE + player.getName());
+                        player.namedTag.putString("rank", "none");
+                    } else  if (args[0].equals("dev")){
+                        player.setNameTag(TextFormat.RED + "[Developer]" + colorMap.get(player.namedTag.getString("chatcolor")) + player.namedTag.getString("nickname"));
+                        player.namedTag.putString("rank", "dev");
+                    } else {
+                        player.setNameTag("[" + args[0] + "]" + colorMap.get(player.namedTag.getString("chatcolor")) + player.namedTag.getString("nickname"));
+                        player.namedTag.putString("rank", args[0]);
+                    }
+                    new NukkitRunnable(){
+                        @Override
+                        public void run(){
+                            player.spawnToAll();
                         }
+                    }.runTaskLater(this, 60);
+                }
+
             }
         }
         if (command.getName().toLowerCase().equals("textcolor")){
             Player player = (Player) sender;
             if (args.length > 0){
+                player.despawnFromAll();
                 if(colorMap.containsKey(args[0])){
                     player.setNameTag(colorMap.get(args[0]) + player.getNameTag());
-                    player.namedTag.putString("chatcolor", args[0].toLowerCase()); 
+                    player.namedTag.putString("chatcolor", args[0].toLowerCase());
                 } else{
                     player.setNameTag(TextFormat.WHITE + player.getNameTag());
-                    player.namedTag.putString("chatcolor", "reset");
+                    player.namedTag.putString("chatcolor", "none");
                 } 
+                new NukkitRunnable(){
+                    @Override
+                    public void run(){
+                        player.spawnToAll();
+                    }
+                }.runTaskLater(this, 60);
             }
         }
-        if (command.getName().toLowerCase().equals("nickname")) {
+        if (command.getName().toLowerCase().equals("nickname")){
             Player player = (Player) sender;
             if (args.length > 0){
-                 player.setNameTag(args[0]);
+                if(args[0].equals("reset")){
+                    player.despawnFromAll();
+                    player.setNameTag(player.getName());
+                    player.namedTag.putString("nickname", player.getName());
+                } else{
+                    player.despawnFromAll();
+                    player.setNameTag(args[0]);
+                    player.namedTag.putString("nickname", args[0]);
+                }
+                new NukkitRunnable(){
+                    @Override
+                    public void run(){
+                        player.spawnToAll();
+                    }
+                }.runTaskLater(this, 60);
             }
         }
         if (command.getName().toLowerCase().equals("deathcount")) {
@@ -135,15 +194,15 @@ public class MainClass extends PluginBase implements Listener{
             }
         }
 
-        return true;
-    }     
+         return true;
+     }     
     
     @EventHandler
-    public void EntityDamageEvent(EntityDamageEvent ev){
+    public void EntityDamageByEntityEvent(EntityDamageByEntityEvent ev){
         if (ev.getEntity() instanceof Player) {
             Player player = (Player) ev.getEntity();
-            if(ev instanceof EntityDamageByEntityEvent){
-                Player damager = (Player)((EntityDamageByEntityEvent) ev).getDamager();
+            if(ev.getDamager() instanceof Player){
+                Player damager = (Player) ev.getDamager();
                 if (player.namedTag.getString("team") == damager.namedTag.getString("team")) {
                     if (player.namedTag.getString("team")!=""){
                     ev.setCancelled();
@@ -177,7 +236,60 @@ public class MainClass extends PluginBase implements Listener{
              }
         }
     }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent ev){
+        Player player = ev.getPlayer();
+        player.despawnFromAll();
+        new NukkitRunnable(){
+        
+            @Override
+            public void run() {
+                String playerName;
+                String chatcolor;
+                String rank;
+        
+                if (player.namedTag.getString("chatcolor").equals(null) || player.namedTag.getString("chatcolor").equals("")){
+                    player.namedTag.putString("chatcolor", "none");
+                    chatcolor = "none";
+                } else{
+                    chatcolor = player.namedTag.getString("chatcolor");
+                }
+        
+                if (player.namedTag.getString("nickname").equals("") || player.namedTag.getString("nickname").equals(null)){
+                    playerName = player.getName();
+                } else{
+                    playerName = player.namedTag.getString("nickname");
+                }
+        
+                if (player.namedTag.getString("rank").equals(null) || player.namedTag.getString("rank").equals("")){
+                    player.namedTag.putString("rank", "none");
+                    rank = "none";
+                }
+                String nametag = colorMap.get(chatcolor) + playerName;
+                if (player.namedTag.getString("team").equals("red")){
+                    nametag = TextFormat.RED + "[Red Team] " + TextFormat.WHITE + playerName;
+                }
+                if (player.namedTag.getString("team").equals("blue")){
+                    nametag = TextFormat.BLUE + "[Blue Team] " + TextFormat.WHITE + playerName;
+                }
+        
+                rank = player.namedTag.getString("rank");
+                if (rank.equals("none") ){
+                } else if (rank.equals("admin")){
+                    nametag = TextFormat.BLUE + "[Admin]" + colorMap.get(chatcolor) + playerName;
+                } else if (rank.equals("dev")){
+                    nametag = TextFormat.RED + "[Developer]" + colorMap.get(chatcolor) + playerName;
+                } else{
+                    nametag = "[" + rank + "]" + colorMap.get(player.namedTag.getString("chatcolor")) + playerName;
+                }
+                player.setNameTag(nametag);
+                player.setDisplayName(nametag);
+                player.spawnToAll();
+            }
+        }.runTaskLater(this, 300);
+        
 
+    }
     private void broadcastMessage(String message){
         Set<CommandSender> recipients = new HashSet<>();
         for (Player playerRecipient : this.getServer().getOnlinePlayers().values()) {
